fa2987d0dae0a548e37ff7f2925ba3559256fb75
DragonXDev <amaardevx@gmail.com>
2025-03-18 12:20:11 -0700

    Add ShellSort class to implement and demonstrate shell sort algorithm

---
+++ sorting_algorithms/ShellSort.java
@@ -0,0 +1,32 @@
+package sorting_algorithms;
+
+import java.util.Arrays;
+
+public class ShellSort {
+    private static void insertionSortInterleaved(int[] numbers, int startIndex, int gap) {
+        for (int i = startIndex + gap; i < numbers.length; i += gap) {
+            int temp = numbers[i];
+            int j = i;
+            while (j - gap >= startIndex && numbers[j - gap] > temp) {
+                numbers[j] = numbers[j - gap];
+                j -= gap;
+            }
+            numbers[j] = temp;
+        }
+    }
+
+    public static void shellSort(int[] numbers, int[] gapValues) {
+        for (int gap : gapValues) {
+            for (int i = 0; i < gap; i++) {
+                insertionSortInterleaved(numbers, i, gap);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = { 55, 99, 87, 54, 31, 17, 28, 18 };
+        int[] gapValues = { 4, 2, 1 };
+        shellSort(numbers, gapValues);
+        System.out.println(Arrays.toString(numbers));
+    }
+}
